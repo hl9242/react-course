c3a1656c614fcc3f0b47d34588b93c63755f9d7a
Harish <hl9242@gmail.com>
2024-04-29 17:10:58 +0530

    episod-03

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React "
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+//React Element
+const jsxHeading = (
+  <h1 className="heading" tabIndex={1}>
+    This is React Heading using jsxHeading 🚀
+  </h1>
+);
+//? React Component
+//? Class based component - Old
+//? Functional Component -New
+const Title = () => (
+   <h1 className="head">React using Title!!!</h1>
+);
+const number = 100;
+const HeadingComponent = () => (
+  <div id="container">
+    <span> {Title()}</span>
+    <Title />
+    <h1 className="heading"> This is Fuctional Component 🚀</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); //? root element of html
+root.render(<HeadingComponent />);
